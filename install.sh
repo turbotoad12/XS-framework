7e61abaf8193dad4a661c4aa6160d6944623cb20
Zachary Jones <jones_zachary@outlook.com>
2025-09-13 15:16:34 -0500

    Add installation script for XS Framework

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+DEVKITPRO=${DEVKITPRO:-/opt/devkitpro}
+PORTLIBS="$DEVKITPRO/portlibs/3ds"
+
+echo "Building XS Framework..."
+make clean
+make
+
+echo "Installing XS Framework to $PORTLIBS"
+
+# Create dirs
+mkdir -p "$PORTLIBS/include"
+mkdir -p "$PORTLIBS/lib"
+
+# Copy headers into the portlibs include folder
+sudo cp -r include/* /opt/devkitpro/portlibs/3ds/include/
+
+# Copy your static library
+sudo cp lib/libxs-framework.a /opt/devkitpro/portlibs/3ds/lib/
+
+
+echo "XS Framework installed!"
+echo "Use with: #include <xs-framework.h> and -lxs-framework"
